56de0952bee7b4b04e3e9eb1af14eaec369c8ed3
Ryoma Ke <kalryoma@gmail.com>
2018-04-29 23:18:53 +1000

    calculate accuracy for prediction

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ def get_label(label_file, chunksize):
         label_dict[name] = label_list.index(label)
     return label_list, label_dict
 
-def get_data(data_file, chunksize):
+def get_data(label_file, data_file, chunksize):
+    label_list, label_dict = get_label(label_file, chunksize)
     x = []
     y = []
     for chunk in pd.read_csv(data_file, header=None, chunksize=chunksize):
         for row in chunk.values:
             name = row[0]
             t = np.zeros(13626)
-            # t[0] = label_dict[name]
+            # t[0] = label_dict[name]+1
             t[0] = 1
             t = np.ndarray(shape=(13626, 1), buffer=t)
             t = csr_matrix(t)
@@ -36,7 +37,7 @@ def get_data(data_file, chunksize):
             data = csr_matrix(data)
             x.append(data)
     y = np.asarray(y)[np.newaxis]
-    return np.asarray(x), y.T
+    return np.asarray(x), y.T, len(label_list)
 
 def generate_feature_mat(x, p):
     result = []
@@ -88,15 +89,48 @@ def linear_regression(x, y, p, lmd):
     w = get_w(ph, y, regularisation)
     return w, ph
 
+def get_class(label_list_length, predicted_result):
+    predicted_label = []
+    l = predicted_result.shape[0]
+    for i in range(l):
+        xi = predicted_result[i][0]
+        min_dist = 100000
+        this_label = 0
+        for j in range(label_list_length):
+            dist = calc_edist(xi, j+1)
+            if dist < min_dist:
+                min_dist = dist
+                this_label = j+1
+        predicted_label.append(this_label)
+    return predicted_label
+
+def calc_edist(a, j):
+    x1 = a[0, 0]
+    distsum = (x1-j)**2
+    start = 1 if a.data[0] == x1 else 0
+    for i in range(start, len(a.data)):
+        distsum += a.data[i]**2
+    return distsum
+
+def calc_accuracy(actual_t, label_list_length, predicted_result):
+    predicted_label = get_class(label_list_length, predicted_result)
+    trueNo = 0
+    for i in range(len(actual_t)):
+        if y[i][0].data[0] == predicted_label[i]:
+            trueNo += 1
+        # print(y[i][0].data[0], predicted_label, y[i][0].data[0] == predicted_label[i])
+    return trueNo*100.0/len(actual_t)
+
 data_file = '/Users/kalryoma/Downloads/5318Assignment1_Data/sample_data.csv'
 label_file = '/Users/kalryoma/Downloads/5318Assignment1_Data/sample_labels.csv'
 chunksize = 50
-label_list, label_dict = get_label(label_file, chunksize)
-x, y = get_data(data_file, chunksize)
+x, y, label_list_length = get_data(label_file, data_file, chunksize)
 
 p = 3
 lmd = 0.1
 w, ph = linear_regression(x, y, p, lmd)
 print(w)
 
-result_test = matrix_mul(ph, w, False)
+predicted_result = matrix_mul(ph, w, False)
+accuracy = calc_accuracy(y, label_list_length, predicted_result)
+print(accuracy)
