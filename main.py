1d66b5cc333d4ea2c65931203b622e7de4f17048
Ryoma Ke <kalryoma@gmail.com>
2018-04-30 02:18:31 +1000

    normalise input training data

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
+from math import exp, sqrt
 
+def normalise(a, max):
+    return a*1.0/max
 def get_label(label_file, chunksize):
     label_list = set()
     label_dict = dict()
     label_name = set()
-    for chunk in pd.read_csv(label_file, header=None, chunksize=chunksize):
+    for chunk in pd.read_csv(label_file, header=None, chunksize=3000):
         for row in chunk.values:
             name = row[0]
             label = row[1]
@@ -20,29 +23,34 @@ def get_label(label_file, chunksize):
 
 def get_data(label_file, data_file, chunksize):
     label_list, label_dict = get_label(label_file, chunksize)
+    label_list_length = len(label_list)
     x = []
     y = []
+    x_max = 0
     for chunk in pd.read_csv(data_file, header=None, chunksize=chunksize):
         for row in chunk.values:
             name = row[0]
             t = np.zeros(13626)
-            # t[0] = label_dict[name]+1
-            t[0] = 1
+            t[0] = normalise(label_dict[name], label_list_length-1)
             t = np.ndarray(shape=(13626, 1), buffer=t)
             t = csr_matrix(t)
             y.append(t)
             data = np.array(row[1:]).astype(np.float)
-            data = np.around(data, 3)
+            data_max = np.max(data)
+            if data_max > x_max:
+                x_max = data_max
+            data = np.around(data, 8)
             data = np.ndarray(shape=(13626, 1), buffer=data)
             data = csr_matrix(data)
             x.append(data)
     y = np.asarray(y)[np.newaxis]
-    return np.asarray(x), y.T, len(label_list)
+    return np.asarray(x), y.T, label_list_length, x_max
 
-def generate_feature_mat(x, p):
+def generate_feature_mat(x_max, x, p):
     result = []
     for xi in x:
         resi = []
+        xi = normalise(xi, x_max)
         for i in range(0, p+1):
             yi = xi.power(i)
             resi.append(yi)
@@ -75,60 +83,63 @@ def generate_regularisaztion_mat(p, lmd):
         regularisation.append(lmd)
     return np.diag(regularisation)
 
-def get_w(ph, y, regularisation):
-    w = matrix_mul(ph.T, ph, True)
-    w = w+regularisation
+def get_w(ph, y, p, lmd):
+    w = matrix_mul(ph.T, ph, True)+generate_regularisaztion_mat(p, lmd)
     w = np.linalg.pinv(w)
     t = matrix_mul(ph.T, y, True)
-    w = np.dot(w, t)
-    return w
+    print(t)
+    return np.dot(w, t)
 
-def linear_regression(x, y, p, lmd):
-    ph = generate_feature_mat(x, p)
-    regularisation = generate_regularisaztion_mat(p, lmd)
-    w = get_w(ph, y, regularisation)
-    return w, ph
-
-def get_class(label_list_length, predicted_result):
+def get_category_index(label_list_length, predicted_result):
     predicted_label = []
     l = predicted_result.shape[0]
     for i in range(l):
         xi = predicted_result[i][0]
         min_dist = 100000
-        this_label = 0
+        this_label = 1
         for j in range(label_list_length):
-            dist = calc_edist(xi, j+1)
+            norm_j = normalise(j, label_list_length-1)
+            dist = calc_edist(xi, norm_j)
             if dist < min_dist:
                 min_dist = dist
-                this_label = j+1
+                this_label = norm_j
         predicted_label.append(this_label)
     return predicted_label
 
-def calc_edist(a, j):
+def calc_edist(a, k):
     x1 = a[0, 0]
-    distsum = (x1-j)**2
-    start = 1 if a.data[0] == x1 else 0
-    for i in range(start, len(a.data)):
-        distsum += a.data[i]**2
-    return distsum
+    distsum = (x1-k)**2
+    data_length = len(a.data)
+    if data_length==0:
+        return sqrt(distsum)
+    else:
+        start = 1 if a.data[0] == x1 else 0
+        for i in range(start, data_length):
+            distsum += a.data[i]**2
+        return sqrt(distsum)
 
 def calc_accuracy(actual_t, label_list_length, predicted_result):
-    predicted_label = get_class(label_list_length, predicted_result)
+    predicted_label = get_category_index(label_list_length, predicted_result)
     trueNo = 0
     for i in range(len(actual_t)):
-        if y[i][0].data[0] == predicted_label[i]:
+        try:
+            actual = actual_t[i][0].data[0]
+        except IndexError:
+            actual = 0
+        if actual == predicted_label[i]:
             trueNo += 1
-        # print(y[i][0].data[0], predicted_label, y[i][0].data[0] == predicted_label[i])
+        # print(actual, predicted_label[i], actual == predicted_label[i])
     return trueNo*100.0/len(actual_t)
 
 data_file = '/Users/kalryoma/Downloads/5318Assignment1_Data/sample_data.csv'
-label_file = '/Users/kalryoma/Downloads/5318Assignment1_Data/sample_labels.csv'
+label_file = '/Users/kalryoma/Downloads/5318Assignment1_Data/training_labels.csv'
 chunksize = 50
-x, y, label_list_length = get_data(label_file, data_file, chunksize)
+x, y, label_list_length, x_max = get_data(label_file, data_file, chunksize)
 
 p = 3
-lmd = 0.1
-w, ph = linear_regression(x, y, p, lmd)
+lmd = exp(-2)
+ph = generate_feature_mat(x_max, x, p)
+w = get_w(ph, y, p, lmd)
 print(w)
 
 predicted_result = matrix_mul(ph, w, False)
